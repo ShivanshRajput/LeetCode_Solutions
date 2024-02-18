eac9008f93da90b231819d82fce986e85492a420
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-02-18 17:33:04 +0530

    Time: 79 ms (13.36%) | Memory: 19.3 MB (37.34%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -1,26 +1,41 @@
 class Solution {
 public:
-    int pickup(int i , int j1 , int j2 , int r , int c , vector<vector<int>>&grid , vector<vector<vector<int>>> &dp){
-        if(j1<0 or j1>=c or j2<0 or j2>=c) return -1e6;
-        if(i==r-1){
-            if(j1==j2) return grid[i][j1];
-            else return grid[i][j1] + grid[i][j2];
-        }
-        if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
-        int maxi=-1e6;
-        for(int dj1=-1;dj1<=1;dj1++){
-            for(int dj2=-1;dj2<=1;dj2++){
-                if(j1==j2) maxi=max(maxi ,grid[i][j1] + pickup(i+1, j1+dj1,j2+dj2,r,c,grid ,dp));
-                else maxi=max(maxi ,grid[i][j1] + grid[i][j2] + pickup(i+1, j1+dj1,j2+dj2,r,c,grid,dp));
+    int maximumCherry(int r, int c, vector<vector<int>> &grid) {
+        vector<vector<int>> front(c, vector<int>(c,-1));
+        for(int j1=0;j1<c;j1++){
+            for(int j2=0;j2<c;j2++){
+                if(j1==j2){
+                    front[j1][j2]=grid[r-1][j1];
+                }
+                else{
+                    front[j1][j2]=grid[r-1][j1] + grid[r-1][j2];
+                }
             }
         }
-        return dp[i][j1][j2]=maxi;
+        for(int i=r-2;i>=0;i--){
+            vector<vector<int>> curr (c, vector<int>(c,-1));
+            for(int j1=0;j1<c;j1++){
+                for(int j2=0;j2<c;j2++){
+                    int maxi=-1e6;
+                    for(int dj1=-1;dj1<=1;dj1++){
+                        for(int dj2=-1;dj2<=1;dj2++){
+                            if(j1+dj1>=0 and j1+dj1<=c-1 and j2+dj2>=0 and j2+dj2<=c-1){
+                                if(j1==j2) maxi = max(maxi , grid[i][j1] + front[j1+dj1][j2+dj2]);
+                                else  maxi=max(maxi ,grid[i][j1] + grid[i][j2] + front[j1+dj1][j2+dj2]);
+                            }
+                        }
+                    }
+                    curr[j1][j2] = maxi;
+                }
+            }
+            front = curr;
+        }
+        return front[0][c-1];
     }
 
     int cherryPickup(vector<vector<int>>& grid) {
         int r=grid.size();
         int c=grid[0].size();
-        vector<vector<vector<int>>> dp(r , vector<vector<int>>(c, vector<int>(c,-1)));
-        return pickup(0 , 0 , c-1 , r , c , grid ,dp);
+        return maximumCherry(r,c,grid);
     }
 };
