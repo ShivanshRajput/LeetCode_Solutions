aac2fbc45f4a5e6d0957cc7c0cc081b7b5f4afb0
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-02-20 22:23:06 +0530

    Time: 3 ms (87.54%) | Memory: 11 MB (49.34%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
-    int minsum(int i , int j , vector<vector<int>> &triangle , vector<vector<int>> &dp){
-        int n = triangle.size();
-        if(i==n-1) return triangle[n-1][j];
-        if(dp[i][j]!=-1) return dp[i][j];
-        int down = triangle[i][j] + minsum(i+1 , j , triangle,dp);
-        int diag = triangle[i][j] + minsum(i+1 , j+1 , triangle,dp);
-        return dp[i][j] = min(down , diag);
-    }
-
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n , vector<int>(n,-1));
-        return minsum(0,0, triangle,dp);
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                triangle[i][j] += min(triangle[i+1][j] , triangle[i+1][j+1]);
+            }
+        }
+        return triangle[0][0];
     }
 };
