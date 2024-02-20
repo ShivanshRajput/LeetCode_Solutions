92014b44948313fa0aaeda4ef83585561ff05eb3
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-02-20 17:21:26 +0530

    Time: 11 ms (89.77%) | Memory: 6.4 MB (77.76%) - LeetSync

--- 268-missing-number/missing-number.c
+++ 268-missing-number/missing-number.c
@@ -1,7 +1,7 @@
 int missingNumber(int* nums, int n) {
-    int sum = 0;
+    int ans = 0;
     for(int i=0;i<n;i++){
-        sum+=nums[i];
+        ans^=(i+1)^nums[i];
     }
-    return n*(n+1)/2 - sum;
+    return ans;
 }
