5f5e7269a4c4b4de3c1cdb144ea3c69a5734a7e0
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-02-20 23:46:40 +0530

    Time: 8 ms (99.44%) | Memory: 13.1 MB (82.34%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
-    bool solve(int i , int target , vector<int>& nums , vector<vector<int>>&dp){
-        if(target == 0) return true;
-        if(i==0) return target==nums[0];
-        if(dp[i][target] != -1) return dp[i][target];
-        bool notpick = solve(i-1 , target , nums,dp);
-        bool pick = false;
-        if(nums[i] <= target){
-            pick = solve(i-1 , target - nums[i] , nums,dp);
-        }
-        return dp[i][target] = pick or notpick;
-    }
-
     bool canPartition(vector<int>& nums) {
-        int sum = accumulate(nums.begin() , nums.end() , 0);
-        if(sum&1) return false;
-        int target = sum/2;
-        int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(target+1,-1));
-        return solve(n-1 , target , nums ,dp);
+        bitset<10001> dp(1);         // declare bitset of size 10001 named 'dp' with decimal value equivalent to 1 (i.e. lsb = 1)
+        int sum=0;
+        for(int &x:nums){
+            sum+=x;
+            dp = dp | (dp<<x);        // exactly similar to not_pick or pick 'x' in nums
+        }
+        if(sum&1) return false;       // if sum is odd, partition cant be made.
+        return dp[sum/2];              // if sum is even, target is to reach half of total sum.
     }
 };
