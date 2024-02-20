53ef11361fb1538b4ca2663353843fc8d723c157
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-02-20 22:09:22 +0530

    Time: 10 ms (32.36%) | Memory: 12.2 MB (43.12%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-    int minSum(int i , int j , vector<vector<int>>& grid , vector<vector<int>> &dp){
-        if(i==0 and j==0) return grid[0][0];
-        if(i<0 or j<0) return 1e5;
-        if (dp[i][j] != -1) return dp[i][j];
-        int up = grid[i][j]+minSum(i-1,j,grid,dp);
-        int left = grid[i][j]+minSum(i,j-1,grid,dp);
-        return dp[i][j] = min(up,left);
-    }
-
+    
     int minPathSum(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>> dp (n,vector<int>(m,-1));
-        return minSum(n-1,m-1 , grid ,dp);
+        vector<int>prevRow(m,-1);
+        for(int i=0;i<n;i++){
+            vector<int>currRow(m,-1);
+            for(int j=0;j<m;j++){
+                if(i==0 and j==0) currRow[j] = grid[i][j];
+                else{
+                    int down=1e8, right=1e8;
+                    if(i>0) down=grid[i][j] + prevRow[j];
+                    if(j>0) right=grid[i][j] + currRow[j-1];
+                    currRow[j]=min(down,right);
+                }
+            }
+            prevRow = currRow;
+        }
+        return prevRow[m-1];
     }
 };
