29e659e6f7d1aea34dfcd93a80b7f702a54c9b61
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-03-01 17:20:49 +0530

    Time: 15 ms (51.39%) | Memory: 15.2 MB (52.80%) - LeetSync

---
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int lcs(string s, string t){
+    int n=s.size();
+    int m=t.size();
+    vector<int> prev(m+1 , 0);
+    for(int i=1;i<=n;i++){
+        vector<int> curr(m+1 , 0);
+        for(int j=1;j<=m;j++){
+            if(s[i-1]==t[j-1]) curr[j] = 1 + prev[j-1];
+            else curr[j] = max(prev[j] , curr[j-1]);
+        }
+        prev = curr;
+    }
+    return prev[m];
+    }
+
+    int minDistance(string word1, string word2) {
+        int _lcs = lcs(word1 ,word2);
+        int _del1 = word1.size() - _lcs;
+        int _del2 = word2.size() - _lcs;
+        return _del1 + _del2;
+    }
+};
