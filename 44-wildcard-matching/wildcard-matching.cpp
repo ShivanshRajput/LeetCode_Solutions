06c973b09981da0a8f161b72f81e7ef61f8e84d2
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-03-03 14:42:35 +0530

    Time: 57 ms (44.31%) | Memory: 14 MB (75.99%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    bool check(int i, int j, string &s , string &t , vector<vector<short>> &dp){
-        if(i<0){
-            if(j<0) return true;
-            else return false;
-        }
-        if(j<0){
-            for(int x=0;x<=i;x++){
-                if(s[x]!='*') return false;
-            }
-            return true;
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==t[j] or s[i]=='?'){
-            return dp[i][j] = check(i-1,j-1,s,t,dp);
-        }
-        if(s[i]=='*'){
-            return dp[i][j] = check(i,j-1,s,t,dp) or check(i-1,j,s,t,dp);
-        }
-        return dp[i][j] = false;
-    }
-
     bool isMatch(string t, string s) {
         int n=s.size();
         int m=t.size();
-        vector<vector<short>> dp(n,vector<short>(m,-1));
-        return check(n-1,m-1,s,t,dp);
+        vector<vector<bool>> dp(n+1,vector<bool>(m+1,0));
+        dp[0][0]=1;
+        for(int i=1;i<=n;i++){
+            if(s[i-1]!='*'){
+                break;
+            }
+            else{
+                dp[i][0]=1;
+            }
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1] or s[i-1]=='?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(s[i-1]=='*'){
+                    dp[i][j] = dp[i][j-1] or dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
