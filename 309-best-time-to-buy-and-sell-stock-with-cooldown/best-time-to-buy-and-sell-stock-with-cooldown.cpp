c8acd1138d5f749154da2c7f37d862f6dc6c4518
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-03-04 23:23:20 +0530

    Time: 3 ms (73.18%) | Memory: 13.4 MB (71.12%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    int earn(int idx , int buy , vector<int>&prices , int n , vector<vector<int>>&dp){
-        if(idx>=n) return 0;
-        if(dp[idx][buy] != -1) return dp[idx][buy];
-        if(buy){
-            return dp[idx][buy] = max(-prices[idx] + earn(idx+1, 0, prices, n,dp) , 0 + earn(idx+1, 1, prices, n,dp));
-        }
-        else{
-            return dp[idx][buy] = max(+prices[idx] + earn(idx+2, 1, prices, n,dp) , 0 + earn(idx+1, 0, prices, n,dp));
-        }
-    }
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        vector<vector<int>> dp(n+1,vector<int>(2,-1));
-        return earn(0,1,prices,n,dp);
+        int n = prices.size();
+        vector<int>great(2,0) , next(2,0) , curr(2,0);
+        for(int idx=n-1;idx>=0;idx--){
+            for(int buy=0;buy<2;buy++){
+                if(buy){
+                    curr[buy] = max(-prices[idx] + next[0] , next[1]);
+                }
+                else{
+                    curr[buy] = max(prices[idx] + great[1] ,  next[0]);
+                }
+            }
+            great = next;
+            next = curr;
+        }
+        return next[1];
     }
 };
