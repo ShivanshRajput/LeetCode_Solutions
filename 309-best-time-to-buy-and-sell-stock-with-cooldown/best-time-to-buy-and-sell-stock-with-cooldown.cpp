766d9e1bb7b16d802e64a38c003114cde6201bc8
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-03-04 23:27:24 +0530

    Time: 6 ms (30.98%) | Memory: 14.3 MB (21.81%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -4,14 +4,8 @@ public:
         int n = prices.size();
         int dp[5002][2]={};
         for(int idx=n-1;idx>=0;idx--){
-            for(int buy=0;buy<2;buy++){
-                if(buy){
-                    dp[idx][buy] = max(-prices[idx] + dp[idx+1][0] , dp[idx+1][1]);
-                }
-                else{
-                    dp[idx][buy] = max(prices[idx] + dp[idx+2][1] ,  dp[idx+1][0]);
-                }
-            }
+            dp[idx][1] = max(-prices[idx] + dp[idx+1][0] , dp[idx+1][1]);
+            dp[idx][0] = max(prices[idx] + dp[idx+2][1] ,  dp[idx+1][0]);
         }
         return dp[0][1];
     }
