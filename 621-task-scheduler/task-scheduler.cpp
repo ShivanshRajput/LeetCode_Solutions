1fef33d7b86f35550d18aeeb9bd097744287b0d3
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-03-19 13:39:52 +0530

    Time: 51 ms (80.04%) | Memory: 37.9 MB (94.23%) - LeetSync

---
+++ 621-task-scheduler/task-scheduler.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+
+        int freq[26]={};
+        for(char t:tasks){
+            freq[t-'A']++;
+        }
+        sort(freq , freq+26);
+        int chunk = freq[25] - 1;
+        int idel = chunk * n;
+        for(int i=24;i>=0;i--){
+            idel -= min(chunk , freq[i]);
+        } 
+        if(idel < 0){
+            return tasks.size();
+        }
+        return tasks.size() + idel;
+    }
+};
