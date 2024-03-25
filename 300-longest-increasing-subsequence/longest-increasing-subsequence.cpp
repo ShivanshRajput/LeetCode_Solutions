3f284e064432924399f4bf4e8eb8509bbe395769
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-03-25 22:15:08 +0530

    Time: 5 ms (89.94%) | Memory: 12.8 MB (63.61%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    int len(int idx , int last_idx , vector<int> &arr , int n , vector<vector<int>>&dp){
-        if(idx<0) return 0;
-        if(dp[idx][last_idx] != -1) return dp[idx][last_idx];
-        if(last_idx==n or arr[idx]<arr[last_idx]){
-            return dp[idx][last_idx] = max(1 + len(idx-1, idx, arr , n,dp) , 0 + len(idx-1, last_idx, arr , n,dp));
-        }
-        else{
-            return dp[idx][last_idx] = len(idx-1, last_idx, arr , n,dp);
-        }
-    }
-
     int lengthOfLIS(vector<int>& nums) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(nullptr);
-        int n = nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1 , -1));
-        return len(n-1 , n , nums , n ,dp);
+        // ios_base::sync_with_stdio(false);
+        // cin.tie(nullptr);    
+        vector<int> temp;
+        for(int &x:nums){
+            auto it = lower_bound(temp.begin() , temp.end() , x);
+            if(it==temp.end()){
+                temp.push_back(x);
+            }
+            else{
+                *it = x;
+            }
+        }
+        return temp.size();
     }
 };
