9a2f92887dc0c9a73b76812e89161c67491468b5
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-03-26 15:59:57 +0530

    Time: 86 ms (43.03%) | Memory: 27.7 MB (37.84%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -21,9 +21,6 @@ public:
     }
 
     int longestStrChain(vector<string>& words) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(nullptr);
-        cout.tie(nullptr);
         int n = words.size();
         int maxi =0;
         vector<int>dp(n , 1);
