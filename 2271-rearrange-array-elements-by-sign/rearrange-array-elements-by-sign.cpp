2bdfa532e4c3804c196f5f018d4ad7de2c0e9503
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-03-27 19:41:41 +0530

    Time: 72 ms (99.89%) | Memory: 125.7 MB (99.85%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
+        ios_base::sync_with_stdio(false); cin.tie(NULL);
         int n=nums.size();
         vector<int> ans(n);
         int pos_index=0 , neg_index=1;
-        for(int i=0;i<n;i++){
-            if(nums[i]<0){
-                ans[neg_index] = nums[i];
+        for(int&i:nums){
+            if(i<0){
+                ans[neg_index] = i;
                 neg_index+=2;
             }
             else{
-                ans[pos_index] = nums[i];
+                ans[pos_index] = i;
                 pos_index+=2;
             }
         }
