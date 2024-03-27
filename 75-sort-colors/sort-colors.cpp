878970f3cce9e378f4b0f5e745e8259e1545986c
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-03-28 01:09:43 +0530

    Time: 4 ms (31.25%) | Memory: 10 MB (5.54%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
         int cnt0 = 0 , cnt1=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]==0) cnt0++;
