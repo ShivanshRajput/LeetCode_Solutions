7e6b30b3ceb300ddeb60775709e7db14c4df1674
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-03-28 13:56:01 +0530

    Time: 7 ms (97.24%) | Memory: 26.5 MB (30.33%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-        int winner = nums[0];
+        int winner = nums.front();
         int votes=0;
         for(int&i:nums){
             if(i==winner){
