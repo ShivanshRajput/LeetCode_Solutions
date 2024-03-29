559eaedd31e7218b2f17c2fb15f78c2f9db57618
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-03-29 19:35:44 +0530

    Time: 23 ms (61.00%) | Memory: 22.7 MB (67.94%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -5,12 +5,12 @@ public:
         int n = intervals.size();
         sort(intervals.begin() , intervals.end());
         vector<vector<int>> ans;
-        for(int i=0;i<n;i++){
-            if(ans.empty() or ans.back()[1] < intervals[i][0]){
-                ans.push_back(intervals[i]);
+        for(auto &i:intervals){
+            if(ans.empty() or ans.back()[1] < i[0]){
+                ans.push_back(i);
             }
             else{
-                ans.back()[1] = max(ans.back()[1] , intervals[i][1]);
+                ans.back()[1] = max(ans.back()[1] , i[1]);
             }
         }
         return ans;
