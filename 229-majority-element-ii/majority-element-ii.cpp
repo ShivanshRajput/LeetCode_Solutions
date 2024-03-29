eda73b0349f116b93ca9e269ac7d4f4a8162bcc9
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-03-29 10:42:52 +0530

    Time: 10 ms (50.68%) | Memory: 18.5 MB (47.50%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,13 +1,33 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int> freq;
-        vector<int> ans;
-        int minfreq = nums.size()/3 + 1;
-        for(int &x:nums){
-            freq[x]++;
-            if(freq[x]==minfreq) ans.push_back(x);
+        int cnt1=0 , cnt2=0;
+        int el1=INT_MIN, el2=INT_MIN;
+        for(int &i:nums){
+            if(cnt1==0 and i!=el2){
+                cnt1 = 1;
+                el1 = i;
+            }
+            else if(cnt2==0 and i!=el1){
+                cnt2 = 1;
+                el2 = i;
+            }
+            else if(i==el1) cnt1++;
+            else if(i==el2) cnt2++;
+            else{
+                cnt1--; cnt2--;
+            }
         }
-        return ans;
+        cnt1=0; cnt2=0;
+        vector<int>ls;
+        for(int &i:nums){
+            if(i==el1) cnt1++;
+            if(i==el2) cnt2++;
+        }
+        int mini = nums.size()/3 + 1;
+        if(cnt1>=mini) ls.push_back(el1);
+        if(cnt2>=mini) ls.push_back(el2);
+        sort(ls.begin() , ls.end());
+        return ls;
     }
 };
