fe52b55a328f66940c6886a8502bfc10465a7dc9
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-03-29 10:39:18 +0530

    Time: 3 ms (98.33%) | Memory: 19.6 MB (7.54%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
         unordered_map<int,int> freq;
         vector<int> ans;
         int minfreq = nums.size()/3 + 1;
