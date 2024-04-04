da7f87ac9fc1e7672e6a3d8d7f564ca31c8b4118
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-04-04 20:15:41 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (49.50%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int maxDepth(string s) {
-        ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
         int maxd=0;
         int depth=0;
         for(char &c:s){
