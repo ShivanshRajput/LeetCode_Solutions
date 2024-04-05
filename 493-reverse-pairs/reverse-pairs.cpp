78760749e1130982bfbed32cb660e7fc79c20101
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-04-05 15:09:47 +0530

    Time: 271 ms (69.70%) | Memory: 109.9 MB (66.94%) - LeetSync

---
+++ 493-reverse-pairs/reverse-pairs.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    void merge(vector<int>& arr, int low, int mid, int high){
+        int cnt=0;
+        int i =low, j=mid+1;
+        vector<int> temp(high-low+1);
+        int k=0;
+        while(i<=mid and j<=high){
+            if(arr[i]<=arr[j]){
+                temp[k++] = arr[i++];
+            }
+            else{
+                temp[k++] = arr[j++];
+            }
+        }
+        while(i<=mid){
+            temp[k++] = arr[i++];
+        }
+        while(j<=high){
+            temp[k++] = arr[j++];
+        }
+        for(int i=0;i<high-low+1;i++){
+            arr[low + i] = temp[i];
+        }
+    }
+
+    int countpairs(vector<int>& arr, int low, int mid, int high){
+        int cnt=0;
+        int j=mid+1;
+        for(int i=low;i<=mid;i++){
+            while(j<=high and arr[i]>2*(long long)arr[j]){
+                j++;
+            }
+            cnt+=j-(mid+1);
+        }
+        return cnt;
+    }
+
+    int mergeSort(vector<int>& arr, int low, int high){
+        int cnt=0;
+        if(low<high){
+            int mid = (low+high)/2;
+            cnt+=mergeSort(arr , low,mid);
+            cnt+=mergeSort(arr , mid+1,high);
+            cnt+=countpairs(arr,low,mid,high);
+            merge(arr , low , mid , high);  // yaha pe array distort ho jaegi
+        }
+        return cnt;
+    }
+
+    int reversePairs(vector<int>& nums) {
+        return mergeSort(nums , 0 , nums.size()-1);
+    }
+};
