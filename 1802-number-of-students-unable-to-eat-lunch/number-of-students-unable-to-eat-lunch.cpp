6c3afc6cc31840f380d97983830f7d8b876da7d3
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-04-09 01:21:52 +0530

    Time: 6 ms (6.02%) | Memory: 10.7 MB (79.21%) - LeetSync

---
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        int circle=0 , square=0;
+        for(int x:students){
+            if(x==0) circle++;
+            else square++;
+        }
+        for(int x :sandwiches){
+            if(x==0){
+                circle--;
+                if(circle<0){
+                    circle++;
+                    break; 
+                } 
+            }
+            else{
+                square--;
+                if(square<0){
+                    square++;
+                    break; 
+                }
+            }
+        }
+        return circle + square;
+    }
+};
