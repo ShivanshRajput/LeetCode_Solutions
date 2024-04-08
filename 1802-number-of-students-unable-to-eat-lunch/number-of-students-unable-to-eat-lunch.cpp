11c512eb2b3f925ed1b1a8239606c0aca6aef40a
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-04-09 01:23:24 +0530

    Time: 5 ms (31.24%) | Memory: 10.8 MB (50.13%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
         int circle=0 , square=0;
-        for(int x:students){
+        for(int & x:students){
             if(x==0) circle++;
             else square++;
         }
-        for(int x :sandwiches){
+        for(int & x :sandwiches){
             if(x==0){
                 circle--;
                 if(circle<0){
