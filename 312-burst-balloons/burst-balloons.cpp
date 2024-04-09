cd040fca4e1c84fb8c0e3cc2fed3fcf6736ab49a
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-04-10 00:54:28 +0530

    Time: 108 ms (77.62%) | Memory: 12 MB (56.52%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    int coins(int i , int j , vector<int>&arr , vector<vector<int>> &dp){
-        if(i>j) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int maxpoints = INT_MIN;
-        for(int k=i;k<=j;k++){ // we are following to burst from last .. watch strivers video
-            int points=arr[i-1]*arr[k]*arr[j+1];
-            points+=coins(i , k-1 , arr,dp);
-            points+=coins(k+1 , j , arr,dp); 
-            maxpoints = max(maxpoints , points);
-        }
-        return dp[i][j] = maxpoints;
-    }
     int maxCoins(vector<int>& nums) {
         nums.insert(nums.begin(),1);
         nums.push_back(1);
         int n = nums.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        return coins(1 , n-2 , nums , dp);
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        for(int i=n-2;i>=1;i--){
+            for(int j=1;j<=n-2;j++){
+                if(i>j) continue;
+                int maxpoints = INT_MIN;
+                for(int k=i;k<=j;k++){ // we are following to burst from last .. watch strivers video
+                    int points=nums[i-1]*nums[k]*nums[j+1] + dp[i][k-1] + dp[k+1][j];
+                    maxpoints = max(maxpoints , points);
+                }
+                dp[i][j] = maxpoints;
+            }
+        }
+        return dp[1][n-2];
     }
 };
