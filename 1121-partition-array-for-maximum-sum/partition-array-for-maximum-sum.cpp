4cce03930da0572309c9c0a65e22477b55386084
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-04-12 19:09:50 +0530

    Time: 11 ms (43.50%) | Memory: 10.9 MB (67.60%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    int solve(int i , int n , int  k , vector<int> &arr ,  vector<int> &dp){
-        if(i==n) return 0;
-        if(dp[i]!= -1) return dp[i];
-        int maxyet = arr[i];
-        int maxsum = INT_MIN;
-        for(int j=i;j<i+k && j<n;j++){
-            maxyet = max(maxyet , arr[j]);
-            int currsum = maxyet * (j-i+1) + solve(j+1 , n , k ,arr ,dp);
-            maxsum = max(maxsum , currsum);
-        }
-        return dp[i] = maxsum;
-    }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<int> dp(n+1 , -1);
-        return solve(0 , n, k , arr ,dp);
+        vector<int> dp(n+1 , 0);
+        for(int i=n-1;i>=0;i--){
+            int maxyet = arr[i];
+            int maxsum = INT_MIN;
+            for(int j=i;j<i+k && j<n;j++){
+                maxyet = max(maxyet , arr[j]);
+                int currsum = maxyet * (j-i+1) + dp[j+1];
+                maxsum = max(maxsum , currsum);
+            }
+            dp[i] = maxsum;
+        }
+        return dp[0];
     }
 };
