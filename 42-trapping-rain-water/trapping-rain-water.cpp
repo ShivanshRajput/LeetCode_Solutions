70c5986e5f1e82efae66ba1e7c7d722de5df760f
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-04-12 19:26:49 +0530

    Time: 4 ms (93.97%) | Memory: 22.5 MB (42.93%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
+        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
         int n = height.size();
 
         int lmax[n];
