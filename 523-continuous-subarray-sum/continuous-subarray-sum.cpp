5a05da3526b2039fe69bf6fa8d907101d287b76f
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-06-08 13:48:18 +0530

    Time: 126 ms (98.23%) | Memory: 134.7 MB (31.96%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
         unordered_set<int> rem;
         for(int i=1;i<nums.size();i++){
             nums[i]+=nums[i-1];
