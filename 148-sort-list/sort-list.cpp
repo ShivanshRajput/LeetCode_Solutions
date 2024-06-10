27ca6a8308a4f5a3c3a7d9abec535e53509f3e27
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-06-10 15:09:06 +0530

    Time: 140 ms (36.16%) | Memory: 73.5 MB (36.67%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,21 +10,54 @@
  */
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
-        // naive approach 
-        vector<int> arr;
-        ListNode * temp = head;
-        while(temp){
-            arr.push_back(temp->val);
-            temp = temp->next;
+    ListNode *merge2list(ListNode *l1 , ListNode *l2){
+        ListNode * dummyHead = new ListNode(-1);
+        ListNode * move = dummyHead;
+        while(l1 && l2){
+            if(l1->val <= l2->val){
+                move->next = l1;
+                l1 = l1->next;
+                move = move->next;
+            }   
+            else{
+                move->next = l2;
+                l2 = l2->next;
+                move = move->next;
+            }
         }
-        sort(arr.begin() , arr.end());
-        temp = head;
-        for(int i=0;i<arr.size();i++){
-            temp->val = arr[i];
-            temp = temp->next;
+        if(l1){
+            move->next = l1;
+        }
+        else{
+            move->next = l2;
+        }
+        return dummyHead -> next;
+    }
+
+
+    ListNode * findMid(ListNode * head){
+        ListNode * mid = head;
+        ListNode * fast = head;
+        while(fast->next && fast->next->next){
+            mid = mid->next;
+            fast = fast->next->next;
+        }
+        return mid;
+    }
+    ListNode* mergeSort(ListNode* head){
+        if(head->next){
+            ListNode * mid = findMid(head);
+            ListNode * l2 = mid->next;
+            mid->next = nullptr;
+            head = mergeSort(head);
+            l2 = mergeSort(l2);
+            head = merge2list(head , l2);
         }
         return head;
-        
+    }
+
+    ListNode* sortList(ListNode* head) {
+        if(head==nullptr or head->next==nullptr) return head;
+        return mergeSort(head);
     }
 };
