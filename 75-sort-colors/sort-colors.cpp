fae38574e160c121e703c5e9163eac193db006a4
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-06-12 13:39:01 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (5.81%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
+    void sortColors(vector<int>& nums) { 
         ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-        int cnt0 = 0 , cnt1=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0) cnt0++;
-            else if(nums[i]==1)  cnt1++;
+        int_fast8_t cnt0 = 0 , cnt1=0;
+        for(int_fast8_t i=0;i<nums.size();++i){
+            cnt0 += (nums[i]==0);
+            cnt1 += (nums[i]==1);
         }
-        for(int i=0;i<cnt0;i++) nums[i]=0;
-        for(int i=cnt0;i<cnt0+cnt1;i++) nums[i]=1;
-        for(int i=cnt0+cnt1;i<nums.size();i++) nums[i]=2;
+        for(int_fast8_t i=0;i<cnt0;++i) nums[i]=0;
+        for(int_fast8_t i=cnt0;i<cnt0+cnt1;++i) nums[i]=1;
+        for(int_fast8_t i=cnt0+cnt1;i<nums.size();++i) nums[i]=2;
     }
 };
