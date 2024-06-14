11cf738d5286eaa27d75f3537c75a1d4cdac95bc
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-06-15 01:36:47 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (73.00%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.cpp
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     bool judgeCircle(string moves) {
+        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
         int xaxis = 0 , yaxis = 0;
         for(char& c: moves){
-            xaxis+=(c=='U');
-            xaxis-=(c=='D');
-            yaxis+=(c=='R');
-            yaxis-=(c=='L');
+            if(c=='U') xaxis++;
+            else if(c=='D') xaxis--;
+            else if(c=='R') yaxis++;
+            else yaxis--;
         }
         return (xaxis==0 && yaxis==0);
     }
