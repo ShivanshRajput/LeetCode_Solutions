16b64fccb7eef5bc07513ea725b5b17be609d094
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-06-16 14:45:44 +0530

    Time: 10 ms (18.83%) | Memory: 15.8 MB (66.19%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -3,15 +3,12 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        queue<pair<pair<int,int>,int>> q;
-        vector<vector<bool>> visited(n,vector<bool>(m,false));
-        int time = 0;
+        queue<pair<int,int>> q;
         int orange = 0;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j] == 2){
-                    q.push({{i,j},time});
-                    visited[i][j] = true;
+                    q.push({i,j});
                     orange++;
                 }
                 else if(grid[i][j]==1){
@@ -19,29 +16,32 @@ public:
                 }
             }
         }
+        int time =0;
         while(!q.empty()){
-            int i = q.front().first.first;
-            int j = q.front().first.second;
-            int t = q.front().second;
-            if(i>0 and grid[i-1][j]==1 and !visited[i-1][j]){
-                q.push({{i-1,j},t+1});
-                visited[i-1][j] = true;
-            }   
-            if(i<n-1 and grid[i+1][j]==1 and !visited[i+1][j]){
-                q.push({{i+1,j},t+1});
-                visited[i+1][j] = true;
-            } 
-            if(j>0 and grid[i][j-1]==1 and !visited[i][j-1]){
-                q.push({{i,j-1},t+1});
-                visited[i][j-1] = true;
-            }   
-            if(j<m-1 and grid[i][j+1]==1 and !visited[i][j+1]){
-                q.push({{i,j+1},t+1});
-                visited[i][j+1] = true;
+            int k = q.size();
+            while(k--){
+                int i = q.front().first;
+                int j = q.front().second;
+                if(i>0 and grid[i-1][j]==1){
+                    q.push({i-1,j});
+                    grid[i-1][j] = 2;
+                }   
+                if(i<n-1 and grid[i+1][j]==1){
+                    q.push({i+1,j});
+                    grid[i+1][j] = 2;
+                } 
+                if(j>0 and grid[i][j-1]==1){
+                    q.push({i,j-1});
+                    grid[i][j-1] = 2;
+                }   
+                if(j<m-1 and grid[i][j+1]==1){
+                    q.push({i,j+1});
+                    grid[i][j+1] = 2;
+                }
+                q.pop();
+                orange--;
             }
-            time = t;
-            q.pop();
-            orange--;
+            if(!q.empty()) time++;
         }
         if(orange == 0){
             return time;
