2547be736e450e8fcfd5c40529304fa4839f4f48
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-06-18 22:45:05 +0530

    Time: 55 ms (48.07%) | Memory: 47.9 MB (17.53%) - LeetSync

---
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        int jobs = difficulty.size();
+        int workers = worker.size();
+        int maxDifficulty = *max_element(difficulty.begin() , difficulty.end());
+        // declaring array to hold all possible difficulties
+        vector<int> maxProfitUptoDifficulty(maxDifficulty+1,0);
+        for(int i=0;i<jobs;i++){  
+            maxProfitUptoDifficulty[difficulty[i]] = max(maxProfitUptoDifficulty[difficulty[i]] , profit[i]);
+        }
+        // prefix sum (like not exactly sum but you get the notion)
+        for(int i=1;i<=maxDifficulty;i++){
+            maxProfitUptoDifficulty[i] = max(maxProfitUptoDifficulty[i] , maxProfitUptoDifficulty[i-1]);
+        }
+        // calculating total profit using vector 'maxProfitUptoDifficulty'
+        int totalProfit = 0;
+        for(int & strength : worker){   // considering each worker's strength
+            if(strength > maxDifficulty){    // for cases of engineers one day before exam 
+                totalProfit += maxProfitUptoDifficulty[maxDifficulty];
+            }
+            else{                            // for cases of normal days
+                totalProfit += maxProfitUptoDifficulty[strength];
+            }
+        }
+        return totalProfit;
+    }
+};
