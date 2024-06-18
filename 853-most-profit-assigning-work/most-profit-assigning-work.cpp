647a5071d79dda6c9e21047dd337a17580ee075d
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-06-18 22:46:41 +0530

    Time: 32 ms (98.32%) | Memory: 48 MB (17.53%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.cpp
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
         int jobs = difficulty.size();
         int workers = worker.size();
         int maxDifficulty = *max_element(difficulty.begin() , difficulty.end());
