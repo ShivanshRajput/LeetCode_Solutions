48d8216258959e50649e2e69ea14c0235e5de6e0
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-06-28 15:22:30 +0530

    Time: 200 ms (99.66%) | Memory: 115.1 MB (100.00%) - LeetSync

--- 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     long long maximumImportance(int n, vector<vector<int>>& roads) {
-        vector<long long> degree(n);
+        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+        long long degree[50000]={};
         for(auto &it:roads){
             degree[it[0]]++;
             degree[it[1]]++;
         }
-        sort(degree.begin() , degree.end());
+        sort(degree, degree+n);
         long long ans = 0;
         for (int i=0;i<n;i++) {
             ans += ((i+1)*degree[i]);
