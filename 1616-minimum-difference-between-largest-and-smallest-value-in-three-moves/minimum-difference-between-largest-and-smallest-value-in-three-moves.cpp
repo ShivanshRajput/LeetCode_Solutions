99aeec89c2557b4fd87a8923a12b38f35af75ad0
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-07-03 19:42:17 +0530

    Time: 46 ms (97.54%) | Memory: 38.9 MB (5.95%) - LeetSync

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int minDifference(vector<int>& nums) {
-        if(nums.size()<=4){
+        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+        uint_fast32_t n = nums.size();
+        if(n<=4){
             return 0;
         }
-        int n = nums.size();
         sort(nums.begin() , nums.end());
         return min({nums[n-4] - nums[0] ,
                     nums[n-3] - nums[1] , 
