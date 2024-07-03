dd767e1f9ab11e7154289fbda6e9b57629036114
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-07-03 19:39:28 +0530

    Time: 67 ms (66.75%) | Memory: 38.8 MB (15.52%) - LeetSync

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -6,6 +6,9 @@ public:
         }
         int n = nums.size();
         sort(nums.begin() , nums.end());
-        return min({abs(nums[0] - nums[n-4]) , abs(nums[1] - nums[n-3]) , abs(nums[2] - nums[n-2]) , abs(nums[3] - nums[n-1])});
+        return min({nums[n-4] - nums[0] ,
+                    nums[n-3] - nums[1] , 
+                    nums[n-2] - nums[2] , 
+                    nums[n-1] - nums[3]});
     }
 };
