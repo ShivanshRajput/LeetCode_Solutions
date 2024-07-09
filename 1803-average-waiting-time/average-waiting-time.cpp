cc8986179aa0613a671e81bcf99b1199c60934d9
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-07-10 01:23:44 +0530

    Time: 122 ms (98.83%) | Memory: 76.8 MB (23.91%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.cpp
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& customers) {
         ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); 
+        uint_fast64_t n = customers.size();
         uint_fast64_t prev = 0;
         uint_fast64_t tot = 0;
-        for(auto & it : customers){
-            uint_fast64_t in = it[0];
-            uint_fast64_t tm = it[1];
+        for(uint_fast64_t i=0;i<n;i++){
+            uint_fast64_t in = customers[i][0];
+            uint_fast64_t tm = customers[i][1];
             if(prev>in){
                 tm += prev - in;
             }
             tot += tm;
             prev = in + tm;
         }
-        return ((double)tot)/customers.size();
+        return (1.0*tot)/n;
     }
 };
