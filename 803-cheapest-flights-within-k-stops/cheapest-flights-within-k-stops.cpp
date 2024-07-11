9be1cfa100abfa4e4ab7b56b88810d9f26fb0252
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-07-11 22:05:16 +0530

    Time: 14 ms (76.86%) | Memory: 17.1 MB (32.71%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -2,29 +2,32 @@ class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-        vector<pair<int,int>> adj[n];
+        vector<pair<int_fast32_t,int_fast32_t>> adj[n];
         for(auto&it:flights){
             adj[it[0]].push_back({it[1],it[2]});
         }
-        vector<int>cost(n,1e9);
-        queue<pair<int,pair<int,int>>> q;
+        vector<int_fast32_t>cost(n,1e9);
+        queue<pair<int_fast32_t,pair<int_fast32_t,int_fast32_t>>> q;
         q.push({0,{src,0}});
         cost[src] = 0;
         while(!q.empty()){
-            int stops = q.front().first;
-            int node = q.front().second.first;
-            int cst = q.front().second.second;
+            int_fast32_t stops = q.front().first;
+            int_fast32_t node = q.front().second.first;
+            int_fast32_t cst = q.front().second.second;
             q.pop();
             if(stops<=k){
                 for(auto&it:adj[node]){
-                    int adj_node = it.first;
-                    int price = it.second;
+                    int_fast32_t adj_node = it.first;
+                    int_fast32_t price = it.second;
                     if(cost[adj_node] > cst + price){
                         cost[adj_node] = cst + price;
                         q.push({stops+1,{adj_node,cost[adj_node]}});
                     }
                 }
-            }   
+            }
+            else{
+                break;
+            } 
         }
         if(cost[dst]==1e9){
             return -1;
