20333e7b6b085de7f61190130994444b02a53a70
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-07-11 18:58:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (15.48%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -2,23 +2,21 @@ class Solution {
 int i=0;
 public:
     string reverseParentheses(string s) {
+        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
         string ans;
         while(i<s.size()){
             if(s[i]==')'){
                 i++;
                 reverse(ans.begin() , ans.end());
-                // cout<<ans<<endl;
                 return ans;
             }
             else if(s[i]=='('){
                 i++;
                 ans += reverseParentheses(s);
-                // cout<<ans<<endl;
             }
             else{
                 ans += s[i];
                 i++;
-                // cout<<ans<<endl;
             }
         }
         return ans;
