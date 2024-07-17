8a68375f4c2d33634c768d6da35fcf192b739902
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-07-17 15:44:56 +0530

    Time: 50 ms (11.46%) | Memory: 22.7 MB (5.78%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -2,39 +2,40 @@ class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
         vector<vector<int>>mat(n,vector<int>(n,1e9));
+        vector<vector<int>>adj[n];
         for(auto &it:edges){
-            mat[it[0]][it[1]] = it[2];
-            mat[it[1]][it[0]] = it[2];
+            adj[it[0]].push_back({it[1],it[2]});
+            adj[it[1]].push_back({it[0],it[2]});
         }
-        for(int i=0;i<n;i++){
-            mat[i][i] = 0;
-        }
-        vector<int> neighbours(n,0);
-        for(int via = 0;via<n;via++){
-            for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
-                    if(mat[i][via]!=1e9 && mat[via][j]!=1e9
-                    && mat[i][via] + mat[via][j] < mat[i][j]){
-                        mat[i][j] = mat[i][via] + mat[via][j];
+        int mini = 1e9;
+        int minCity = -1;
+        for(int src = 0;src<n;src++){
+            priority_queue<pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>>> min_heap;
+            vector<int> distance(n,1e9);
+            distance[src] = 0;
+            min_heap.push({0,src});      
+            while(!min_heap.empty()){
+                int node = min_heap.top().second;
+                int dist = min_heap.top().first;
+                min_heap.pop();
+                for(auto &it:adj[node]){
+                    if(distance[it[0]] > distance[node] + it[1]){
+                        distance[it[0]] = distance[node] + it[1];
+                        min_heap.push({distance[it[0]] , it[0]});
                     }
                 }
             }
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(mat[i][j]<=distanceThreshold){
-                    neighbours[i]++;
+            int cnt = 0;
+            for(int &it:distance){
+                if(it<=distanceThreshold){
+                    cnt++;
                 }
             }
-        }
-        int minIndex = -1;
-        int mini = 1e9;
-        for(int i=0;i<n;i++){
-            if(neighbours[i]<=mini){
-                mini = neighbours[i];
-                minIndex = i;
+            if(cnt<=mini){
+                mini = cnt;
+                minCity = src;
             }
         }
-        return minIndex;
+        return minCity;
     }
 };
