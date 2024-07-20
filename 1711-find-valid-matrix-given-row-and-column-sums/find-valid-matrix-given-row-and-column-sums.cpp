af9c80e1e55fc43ce0427b0dae88c7335397d409
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-07-21 00:17:43 +0530

    Time: 37 ms (60.27%) | Memory: 36.1 MB (11.51%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.cpp
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.cpp
@@ -4,20 +4,12 @@ public:
         ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
         int n = rowSum.size();
         int m = colSum.size();
-        vector<vector<int>>ans(n , vector<int>(m,0));
-        int i=0,j=0;
-        while(i<n && j<m){
-            if(rowSum[i] <= colSum[j]){
-                ans[i][j] = rowSum[i];
-                colSum[j]-= rowSum[i];
-                rowSum[i]=0;
-                i++;
-            }
-            else{
-                ans[i][j] = colSum[j];
-                rowSum[i]-= colSum[j];
-                colSum[j]=0;
-                j++;
+        vector<vector<int>>ans(n,vector<int>(m));
+        for(int i =0;i<n;i++){
+            for(int j =0;j<m;j++){
+                ans[i][j] = min(rowSum[i],colSum[j]);
+                rowSum[i]-=ans[i][j];
+                colSum[j]-=ans[i][j];
             }
         }
         return ans;
