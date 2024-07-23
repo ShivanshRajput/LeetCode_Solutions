9f60844c37fcad8f12e079266220afd887fa6284
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-07-23 17:39:43 +0530

    Time: 390 ms (31.58%) | Memory: 159 MB (31.74%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.cpp
+++ 854-making-a-large-island/making-a-large-island.cpp
@@ -1,8 +1,7 @@
 class DisjointSet { 
 public:
-    vector<int> rank, parent, size;
-    DisjointSet(int n) {
-        rank.resize(n+1, 0); 
+    vector<int> parent, size;
+    DisjointSet(int n) { 
         parent.resize(n+1);
         size.resize(n+1); 
         for(int i = 0;i<=n;i++) {
@@ -16,23 +15,6 @@ public:
             return node; 
         return parent[node] = findUPar(parent[node]); 
     }
-
-    void unionByRank(int u, int v) {
-        int ulp_u = findUPar(u); 
-        int ulp_v = findUPar(v); 
-        if(ulp_u == ulp_v) return; 
-        if(rank[ulp_u] < rank[ulp_v]) {
-            parent[ulp_u] = ulp_v; 
-        }
-        else if(rank[ulp_v] < rank[ulp_u]) {
-            parent[ulp_v] = ulp_u; 
-        }
-        else {
-            parent[ulp_v] = ulp_u; 
-            rank[ulp_u]++; 
-        }
-    }
-
     void unionBySize(int u, int v) {
         int ulp_u = findUPar(u); 
         int ulp_v = findUPar(v); 
@@ -95,8 +77,9 @@ public:
                 }
             }
         }
-        maxSize = max(maxSize, ds.size[ds.findUPar(0)]); // if all are 1's
-
+        for (int cellNo = 0; cellNo < n * n; cellNo++) {
+            maxSize = max(maxSize, ds.size[ds.findUPar(cellNo)]);
+        }
         return maxSize;
     }
 };
