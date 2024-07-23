d5519d84289e3fd5ee9512a627ac7567e97e92ff
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-07-24 00:26:20 +0530

    Time: 6 ms (58.80%) | Memory: 15 MB (25.59%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -3,8 +3,8 @@ public:
     vector<int> frequencySort(vector<int>& nums) {
         ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
         int_fast16_t freq[201] = {};
-        for(int &i:nums){
-            freq[i+100]++;
+        for(int i=0;i<nums.size();i++){
+            freq[nums[i]+100]++;
         }
         sort(nums.begin() , nums.end() , [&](int_fast16_t a, int_fast16_t b){
             if(freq[a+100] == freq[b+100]){
