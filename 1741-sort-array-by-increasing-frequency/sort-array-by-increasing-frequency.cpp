1100d88b01c1fc74082bb2c36ee6e8aca34cd1b3
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-07-24 00:22:37 +0530

    Time: 9 ms (34.26%) | Memory: 14.8 MB (70.42%) - LeetSync

---
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        int freq[201] = {};
+        for(int &i:nums){
+            freq[i+100]++;
+        }
+        sort(nums.begin() , nums.end() , [&](int a, int b){
+            if(freq[a+100] == freq[b+100]){
+                return a>b;
+            }
+            return freq[a+100]<freq[b+100];
+        } );
+        return nums;
+    }
+};
