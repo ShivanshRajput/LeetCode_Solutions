3497741138114fc1f9b4c23cb733142836174376
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-07-25 01:08:29 +0530

    Time: 11 ms (74.98%) | Memory: 12.6 MB (66.56%) - LeetSync

---
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -0,0 +1,36 @@
+class Solution {
+private:
+    bool isValid(int i,int j,int n, int m){
+        return i>=0 && i<n && j>=0 && j<m;
+    }
+public:
+    int swimInWater(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> time(n , vector<int>(m,1e9));
+        priority_queue<pair<int,pair<int,int>> , vector<pair<int,pair<int,int>>> , greater<pair<int,pair<int,int>>>> pq;
+        pq.push({grid[0][0],{0,0}});
+        time[0][0] = grid[0][0];
+        int dr[4] = {-1,0,1,0};
+        int dc[4] = { 0,1,0,-1};
+        while(!pq.empty()){
+            int curr_time = pq.top().first;
+            int curr_row = pq.top().second.first;
+            int curr_col = pq.top().second.second;
+            pq.pop();
+            if(curr_row==n-1 && curr_col==m-1) return curr_time;
+            for(int ind=0;ind<4;ind++){
+                int adj_row = curr_row + dr[ind];
+                int adj_col = curr_col + dc[ind];
+                if(isValid(adj_row,adj_col,n,m)){
+                    int alter_time = max(time[curr_row][curr_col] , grid[adj_row][adj_col]);
+                    if(alter_time < time[adj_row][adj_col]){
+                        time[adj_row][adj_col] = alter_time;
+                        pq.push({alter_time , {adj_row,adj_col}});
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+};
