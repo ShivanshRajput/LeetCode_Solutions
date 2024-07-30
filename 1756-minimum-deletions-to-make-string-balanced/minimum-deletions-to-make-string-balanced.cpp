c1d115cc035e1861fa76cd8e2f393d839fce0768
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-07-30 18:58:13 +0530

    Time: 42 ms (99.04%) | Memory: 23.8 MB (65.23%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
+        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
         int n =  s.size();
-        vector<int> preB(n) , sufA(n);
-        preB[0] = (s[0] == 'b');
-        sufA[0] = count(s.begin() , s.end() , 'a');
+        int preB = (s[0] == 'b');
+        int sufA = count(s.begin() , s.end() , 'a');
+        int count = preB + sufA - 1;
         for(int i=1;i<n;i++){
-            preB[i]=preB[i-1] + (s[i]=='b');
-            sufA[i]=sufA[i-1] - (s[i-1]=='a');
-        }
-        int count = n;
-        for(int i=0;i<n;i++){
-            count = min(count ,sufA[i] + preB[i] - 1 );
+            preB+=(s[i]=='b');
+            sufA-=(s[i-1]=='a');
+            count = min(count , preB + sufA - 1);
         }
         return count;
     }
