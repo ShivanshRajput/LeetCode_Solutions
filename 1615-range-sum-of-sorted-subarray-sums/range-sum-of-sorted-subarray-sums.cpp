fd49960fa914810cfc0dacd7f96e15e1f7e4ac60
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-08-04 23:45:50 +0530

    Time: 88 ms (71.01%) | Memory: 27.1 MB (44.27%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
@@ -3,15 +3,11 @@ public:
     long long MOD = 1e9 + 7;
     int rangeSum(vector<int>& nums, int n, int left, int right) {
         ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-        vector<int>presum(n+1,0);
-        presum[0] = 0;
-        for(int i=0;i<n;i++){
-            presum[i+1] = presum[i] + nums[i];
-        }
         vector<int> arr;
-        for(int i=0;i<=n;i++){
-            for(int j=i+1;j<=n;j++){
-                int sum = presum[j] - presum[i];
+        for(int i=0;i<n;i++){
+            int sum = 0;
+            for(int j=i;j<n;j++){
+                sum += nums[j];
                 arr.push_back(sum);
             }
         }
@@ -20,7 +16,7 @@ public:
         for(int i=left-1;i<=right-1;i++){
             ans = (ans + arr[i])%MOD;
         }
-        return ans;
+        return ans%MOD;
     }
     
 };
