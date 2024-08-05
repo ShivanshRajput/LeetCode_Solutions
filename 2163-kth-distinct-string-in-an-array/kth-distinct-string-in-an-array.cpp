6fececb190b090415cc5198194d58f377057b4dc
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-08-05 20:46:55 +0530

    Time: 9 ms (94.43%) | Memory: 18.5 MB (20.16%) - LeetSync

--- 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.cpp
+++ 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     string kthDistinct(vector<string>& arr, int k) {
+        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
         unordered_map<string,int> freq;
         for(auto &it: arr){
             freq[it]++;
