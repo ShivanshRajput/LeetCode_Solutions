bce0b7af9eb28e442432948c17385e913c243228
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-08-11 12:29:39 +0530

    Time: 4 ms (79.19%) | Memory: 12.2 MB (39.75%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin() , nums.end());
-        for(int i=1;i<nums.size();i+=3){
-            if(nums[i]!=nums[i-1]){
-                return nums[i-1];
-            }
+        int ones = 0;  // stores xor of all numbers appearing once.. aka our required number
+        int twos = 0;  // stores xor of all numbers appearing twice, we dont need threes
+        for(int &x:nums){
+            ones = (ones^x)&(~twos);   // if the no is not appeared twice, its first occurance
+            twos = (twos^x)&(~ones);   // in second occurance of x, it got deleted from ones, so now if it is not in ones its added in twos
         }
-        return nums.back();
+        return ones;
     }
 };
