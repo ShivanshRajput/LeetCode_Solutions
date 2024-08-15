f3cefc3ffc47f9600f83460f5f9ab6178d104d2b
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-08-15 23:59:25 +0530

    Time: 71 ms (62.76%) | Memory: 86.1 MB (9.36%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int fives = 0 , tens = 0;
+        for (int x : bills) {
+            if (x == 5) {
+                fives++;
+            } else if (x == 10) {
+                if (fives > 0) {
+                    fives--;
+                    tens++;
+                } else {
+                    return false;
+                }
+            } else {
+                if (fives > 0 && tens > 0) {
+                    fives--;
+                    tens--;
+                } else if (fives > 2) {
+                    fives -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+
+    }
+};
