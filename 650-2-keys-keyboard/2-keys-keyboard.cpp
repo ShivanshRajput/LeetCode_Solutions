7ddcaec2c6516965e7602a407baef640ba8c70d9
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-08-20 00:48:38 +0530

    Time: 3 ms (69.17%) | Memory: 7.6 MB (49.13%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -18,6 +18,7 @@ private:
     }
 public:
     int minSteps(int n) {
+        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
         if(n==1) return 0;
         if(dp[n] != 0) return dp[n];
         int factor = findFactor(n);
