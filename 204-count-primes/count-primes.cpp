b639e68cffe5a5db7e9c30695d812bb077b0987a
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-08-25 01:14:47 +0530

    Time: 92 ms (98.70%) | Memory: 12.9 MB (33.63%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int countPrimes(int n) {
+        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
         bool sieve[5000001] = {};
-        for(int i=2;i*i<=n;i++){
+        for(int_fast32_t i=2;i*i<=n;i++){
             if(!sieve[i]){
-                for(int j = i*i;j<=n;j+=i){
+                for(int_fast32_t j = i*i;j<=n;j+=i){
                     sieve[j] = true;
                 }
             }
         }
-        int count = 0;
-        for(int i=2;i<n;i++){
+        int_fast32_t count = 0;
+        for(int_fast32_t i=2;i<n;i++){
             if(!sieve[i]) count++;
         }
         return count;
