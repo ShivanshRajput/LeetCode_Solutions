6d30e0f17c7e0f53933574df0eb3efbd45d73079
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-08-27 21:06:49 +0530

    Time: 192 ms (11.50%) | Memory: 68.9 MB (86.26%) - LeetSync

---
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
+        vector<pair<int,double>> adj[n];
+        for(int i=0;i<edges.size();i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            adj[u].push_back({v , succProb[i]});
+            adj[v].push_back({u , succProb[i]});
+        }
+        vector<double> prob(n , 0.0);
+        priority_queue<pair<double,int>>  max_heap;
+        prob[start] = 1.0;
+        max_heap.push({prob[start], start});
+        while(!max_heap.empty()){
+            double curr_prob = max_heap.top().first;
+            int node = max_heap.top().second;
+            cout<<node<<" "<<curr_prob<<endl;
+            max_heap.pop();
+            for(auto &it: adj[node]){
+                int child = it.first;
+                double child_prob = it.second;
+                double new_prob = curr_prob * child_prob;
+                if(new_prob > prob[child]){
+                    prob[child] = new_prob;
+                    max_heap.push({new_prob , child});
+                }
+            }
+        }
+        return prob[end];
+    }
+};
