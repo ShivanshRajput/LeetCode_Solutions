7af13b57e31cfaadd742ed121581c9c0d60eedbf
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-08-27 21:07:55 +0530

    Time: 151 ms (26.57%) | Memory: 69.1 MB (76.34%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
+        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
         vector<pair<int,double>> adj[n];
         for(int i=0;i<edges.size();i++){
             int u = edges[i][0];
