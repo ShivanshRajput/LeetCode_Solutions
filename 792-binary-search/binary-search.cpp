782ad8825704609c59f9245e6c456d304f2dc54e
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-08-30 01:56:05 +0530

    Time: 7 ms (99.67%) | Memory: 30.2 MB (20.62%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
         int low = 0 , high = nums.size()-1;
         while(low<=high){
             int mid = (low+high)/2;
