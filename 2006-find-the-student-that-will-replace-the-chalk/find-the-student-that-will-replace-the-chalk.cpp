a000ec76d0a25495e07456f3305df173adf6cb62
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-09-02 15:12:51 +0530

    Time: 55 ms (96.49%) | Memory: 76.9 MB (29.23%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
+        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
         int n =  chalk.size();
         long long sum = accumulate(chalk.begin() , chalk.end() , 0ll);
         k%=sum;
