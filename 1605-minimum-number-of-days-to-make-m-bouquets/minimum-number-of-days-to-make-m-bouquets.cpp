8e64ec6a6a9b8659222f6fd9c069f8a1dc844e7a
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-09-05 19:33:48 +0530

    Time: 47 ms (99.94%) | Memory: 69.5 MB (14.00%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     bool bouquetPossible(vector<int>&bloomDay , int m , int k , int day){
-        int total = 0;
-        for (int i = 0; i < bloomDay.size(); i++) {
+        int bouquetCount = 0;
+        int n = bloomDay.size();
+        for (int i = 0; i < n; i++) {
             int count = 0;
-            while (i < bloomDay.size() && count < k && bloomDay[i] <= day) {
+            while (i < n && count < k && bloomDay[i] <= day) {
                 count++;
                 i++;
             }
-            if (count == k) {
-                total++;
+            if (count == k) {  // bouquet formed here
+                bouquetCount++;
                 i--;
             }
-            if (total >= m) {
+            if (bouquetCount >= m) {  // m bouquets are formed 
                 return true;
             }
         }
@@ -21,17 +22,21 @@ public:
 
     int minDays(vector<int>& bloomDay, int m, int k) {
         ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-        if((long long)m*k > bloomDay.size()) return -1;
-        int low = 1 , high = 1e9;
-        while(low<high){
+        int n = bloomDay.size();
+        if((long long)m*k > n) return -1;
+        int low = *min_element(bloomDay.begin() , bloomDay.end());
+        int high = *max_element(bloomDay.begin() , bloomDay.end());
+        int min_days = high;
+        while(low<=high){
             int mid = (low + high)/2;
             if(bouquetPossible(bloomDay , m , k , mid)){
-                high = mid;
+                min_days = mid;
+                high = mid-1;
             }
             else{
                 low = mid+1;
             }
         }
-        return low;
+        return min_days;
     }
 };
