6345fe945411b0dc8983b1a654a519f668f946d5
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-09-05 13:30:53 +0530

    Time: 75 ms (97.30%) | Memory: 116.4 MB (55.88%) - LeetSync

--- 2155-find-missing-observations/find-missing-observations.cpp
+++ 2155-find-missing-observations/find-missing-observations.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
+        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
         int rolled = rolls.size() + n;
         int sum = rolled * mean;
         int known = accumulate(rolls.begin() , rolls.end() , 0);
@@ -14,6 +15,5 @@ public:
             ans[i]++;
         }
         return ans;
-
     }
 };
