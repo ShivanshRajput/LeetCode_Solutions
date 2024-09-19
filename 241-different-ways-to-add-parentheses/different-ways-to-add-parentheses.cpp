871d0734d1f970807701e8906c5eca4c422db08c
Shivansh Rajput <123619826+ShivanshRajput@users.noreply.github.com>
2024-09-19 11:06:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (83.98%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
@@ -1,6 +1,10 @@
 class Solution {
+    unordered_map<string , vector<int>> dp;
 public:
     vector<int> diffWaysToCompute(string expression) {
+        if(dp.find(expression) != dp.end()){
+            return dp[expression];
+        }
         int n = expression.size();
         vector<int> ans;
         for(int i=0;i<n;i++){
@@ -24,6 +28,6 @@ public:
             }
         }
         if(ans.empty()) ans.push_back(stoi(expression));
-        return ans;
+        return dp[expression] = ans;
     }
 };
